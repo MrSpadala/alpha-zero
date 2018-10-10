11e4a9b7d0cba52d552da0584c37bb0816f4f4e6
spadala <pietro.spa@gmail.com>
2018-10-10 17:11:10 +0200

    refactored utils

---
+++ utils/eval.py
@@ -0,0 +1,16 @@
+
+"""
+Evaluation done on selfplay data on iteration 60
+"""
+
+from NNet import NNet
+from pickle import load
+import sys
+
+its = [1,3,5,7,10,13,15,17,20,23,25,27,30,
+33,35,37,40,43,45,47,50,53,55,57,60]
+#its = [60]
+for it in its:
+  net = NNet(str(it))
+  res = net.evaluate(load(open('selfplay_data/examples_it_60.pickle', 'rb')))
+  print(it, res)
